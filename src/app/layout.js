d5014858e3dc756529cbb866af18fef1071dae9a
CheatSetha <cheatsetha2001@gmail.com>
2023-11-22 22:08:12 +0700

    udpate

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,16 +11,17 @@ const darker_groteque = Darker_Grotesque({
 
 export const metadata = {
   title: "Cyber-STAD",
+
   description:
     "Cyber-STAD is a cybersecurity group of student who study at CSTAD. This website is created in order to share what we have learned and what we have done. We also want to share our vision and mission to the world. We hope that we can help people to learn more about cybersecurity and help them to protect themselves from cybercrime. and everyonw can contact us for hire us to do some project.",
   // metadata for social media
   openGraph: {
-    images:  "/images/metadata.jpg",
+    images: "/images/metadata.jpg",
     title: "Cyber-STAD",
     description:
       "Cyber-STAD is a cybersecurity group of student who study at CSTAD. This website is created in order to share what we have learned and what we have done. We also want to share our vision and mission to the world. We hope that we can help people to learn more about cybersecurity and help them to protect themselves from cybercrime. and everyonw can contact us for hire us to do some project.",
     type: "website",
- 
+
     url: "https://cyber-stad.vercel.app",
   },
   twitter: {
